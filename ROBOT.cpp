b72344300f5e327f894bc8e722f6c1a2a6f3421d
Harry <huynhdanghaiduong@gmail.com>
2026-09-09 22:08:31 +0700

    Sửa kết quả đoạn từ l->r

--- ROBOT.cpp
+++ ROBOT.cpp
@@ -11,10 +11,10 @@ int main(){
     }
     while(q--){
         ll l, s; cin >> l >> s;
-        auto it = upper_bound(pre.begin()+l, pre.end(), s+a[l-1]);
+        auto it = upper_bound(pre.begin()+l, pre.end(), s+pre[l-1]);
         if(it!=pre.begin()){
             it--;
-            ll ans=it-pre.begin()-1;
+            ll ans=it-pre.begin()-l+1;
             //cout  << l << " " << it-pre.begin()+l << "\n";
             if(ans<=0) cout << "0\n";
             else cout << ans << "\n";
