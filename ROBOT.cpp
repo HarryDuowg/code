8528ff95cdc9bdc342a6912a3f774f6140b88b97
Harry <huynhdanghaiduong@gmail.com>
2026-09-09 22:02:32 +0700

    Add files via upload

---
+++ ROBOT.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll n, q; cin >> n >> q;
+    vector <ll> a(n+1, 0), pre(n+1, 0);
+    for(ll i=1 ; i<=n ; i++){
+        cin >> a[i];
+        pre[i]=pre[i-1]+a[i];
+    }
+    while(q--){
+        ll l, s; cin >> l >> s;
+        auto it = upper_bound(pre.begin()+l, pre.end(), s+a[l-1]);
+        if(it!=pre.begin()){
+            it--;
+            ll ans=it-pre.begin()-1;
+            //cout  << l << " " << it-pre.begin()+l << "\n";
+            if(ans<=0) cout << "0\n";
+            else cout << ans << "\n";
+        }
+    }
+}
